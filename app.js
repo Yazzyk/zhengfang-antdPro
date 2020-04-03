6ff4c5b2e08b25fd1e160283aa9015529014df84
YangKai <yangkai@kointernet.com>
2020-04-04 02:06:37 +0800

    上线

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const express = require('express');
 const path = require('path');
-// eslint-disable-next-line import/no-extraneous-dependencies
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 app.use(express.static(path.join(__dirname, './dist')));
 app.use(
   '/api',
-  proxy({
+  createProxyMiddleware({
     target: 'https://backstage.edu.css0209.cn/',
     changeOrigin: true,
     pathRewrite: { '^/api': '' },
